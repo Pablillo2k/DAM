0a2ea2539c67198adbad523e511a1ca92eb2f81b
pablillo2k <pablillo2k6@.com>
2024-10-30 18:08:13 +0100

    si

---
+++ Snippets/Snippets.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Snippets {
+    public static void main(String[] args) {
+
+        scn/ msg/ menu
+
+        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("1.-");
+            System.out.println("2.-");
+            System.out.println("3.-");
+            System.out.println("4.-");
+            System.out.println("5.-");
+        opcion = sc.nextInt();
+        switch (opcion) {
+        case 1:
+            // Código opcion 1
+            
+            break;
+            case 2:
+            // Código opcion 2
+            
+            break;
+            case 3:
+            // Código opcion 3
+            
+            break;
+            case 4:
+            // Código opcion 4
+            
+            break;
+            case 5:
+            // Código opcion 5
+            
+            break;
+            default:
+            System.out.println("Opcion no valida");
+        }
+        } 	while (opcion != 5);
+    }
+}
